b92323afa87ef0a082820c681417033ee46c05f5
MDrenova <mateodrenova@gmail.com>
2023-12-14 04:22:46 -0800

    Update betty style code and add comments

--- command_path.c
+++ command_path.c
@@ -1,9 +1,16 @@
 #include "shell.h"
+/**
+ * command_path - function to find the full path of a command
+ * @cmd: The command to find the path for
+ * Return: The full path of the commmand, or Null if an error occurred.
+ */
 
 char *command_path(char *cmd)
 {
 	char *path, *path_copy, *full_path, *token;
+
 	path = _getenv("PATH");
+
 	if (path == NULL)
 	{
 		fprintf(stderr, "Path variable not found");
@@ -12,25 +19,26 @@ char *command_path(char *cmd)
 	path_copy = strdup(path);
 	if (path_copy == NULL)
 	{
-	fprintf(stderr, "Error coping path");
-	return (NULL);
+		fprintf(stderr, "Error coping path");
+		return (NULL);
 	}
 	token = strtok(path_copy, ":");
 	while (token != NULL)
 	{
-		full_path = malloc(strlen(token)+ strlen(cmd)+ 2);
+		full_path = malloc(strlen(token) + strlen(cmd) + 2);
 		if (full_path == NULL)
 		{
-		fprintf(stderr, "Error allocating full path");
-		free(path_copy);
-		return (NULL);
+			fprintf(stderr, "Error allocating full path");
+			free(path_copy);
+			return (NULL);
 		}
-	strcpy(full_path, token);
-	strcat(full_path, "/");
-	strcat(full_path, cmd);
-	
-	token = strtok(NULL, ":");
+		strcpy(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, cmd);
+
+		token = strtok(NULL, ":");
 	}
+
 	free(path_copy);
-	return(full_path);
+	return (full_path);
 }
--- env_utils.c
+++ env_utils.c
@@ -1,36 +1,46 @@
 #include "shell.h"
-
-int _printenv()
+/**
+ * _printenv - function to print the environment variables.
+ *
+ * Return: 0 on success, -1 if an error occurred.
+ */
+int _printenv(void)
 {
 	int i, file_descr = 1;
+
 	if (environ == NULL)
-	{
 		fprintf(stderr, "error");
-	}
 
 	i = 0;
-	while(environ[i] != NULL)
+	while (environ[i] != NULL)
 	{
-		write(file_descr, environ[i],strlen(environ[i]));
+		write(file_descr, environ[i], strlen(environ[i]));
 		write(file_descr, "\n", 1);
 		i++;
 	}
-	return(0);
+
+	return (0);
 }
+/**
+ * _getenv - ffunction to get the value of an environment variable.
+ * @var: the name of environment varaible to get.
+ * Return: the value of the environment variable, or NULL if an error ocurred.
+ */
 char *_getenv(char *var)
-	{
+{
 	int i, len_var;
+
 	if (environ == NULL)
 	{
 		fprintf(stderr, "environmet variable is null");
 		return (NULL);
-	
 	}
 	len_var = strlen(var);
+
 	for (i = 0; environ[i] != NULL; i++)
 	{
-	if (strncmp(environ[i], var, len_var) == 0 )
-		return (&environ[i][len_var]);
+		if (strncmp(environ[i], var, len_var) == 0)
+			return (&environ[i][len_var]);
 	}
 	return (NULL);
 }
