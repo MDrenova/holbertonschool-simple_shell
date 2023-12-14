692100784b0ad308c31ce92728d6f0e348ec69b8
MDrenova <mateodrenova@gmail.com>
2023-12-14 04:27:14 -0800

    Add more prototypes to the header file

--- shell.h
+++ shell.h
@@ -1,18 +1,21 @@
-#ifndef _SHELL_H
-#define _SHELL_H
+#ifndef SHELL_H
+#define SHELL_H
 
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
 extern char **environ;
 
 int _printenv(void);
 char *_getenv(char *var);
 
 char *command_path(char *cmd);
+int command_read(char *s);
+int execute(char *cmd_arr[]);
 
 #endif
