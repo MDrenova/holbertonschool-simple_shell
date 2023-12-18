20776e1800e15fdef339956b5b15cc584eb66f2a
Arbes14 <arbesavdiaj14@gmail.com>
2023-12-18 01:01:42 -0800

    update shell.h

--- shell.c
+++ shell.c
@@ -1,55 +1,50 @@
 #include "shell.h"
 
 /**
- * main - Main function of the shell.
- * @argc: The number of arguments.
- * @argv: The arguments.
- *
- * Return: 0 on success, or the error code on failure.
+ * main - Entry point
+ * @argc: The number of arguments
+ * @argv: The arguments
+ * Return: 0 on success, 1 on failure
  */
-int main(void)
+
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	char *argv[2];
-	pid_t pid;
+	size_t buf_size = 0;
+	ssize_t characters = 0;
 
-	while (1) {
-		printf("#cisfun$ ");
-		read = getline(&line, &len, stdin);
-		if (read == -1) {
-			printf("\n");
-			exit(EXIT_SUCCESS);
+	name = argv[0];
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) == 1)
+			write(1, "$ ", 2);
+		characters = getline(&line, &buf_size, stdin);
+		if (characters == -1)
+		{
+			if (isatty(STDIN_FILENO) == 1)
+				write(1, "\n", 1);
+			break;
 		}
-		line[read - 1] = '\0';  /* Remove newline character */
-
-		pid = fork();
-		if (pid == -1) {
-			perror("Error:");
-			return (1);
-		}
-		if (pid == 0) {
-			argv[0] = line;
-			argv[1] = NULL;
-			if (execve(line, argv, NULL) == -1) {
-				perror("./shell");
-			}
-		} else {
-			wait(NULL);
-		}
+		if (line[characters - 1] == '\n')
+			line[characters - 1] = '\0';
+		if (*line == '\0')
+			continue;
+		if (strcmp(line, "exit") == 0)
+			break;
+		if (command_read(line) == 2)
+			break;
 	}
 	free(line);
 	return (0);
 }
 
 /**
- * command_read - Function to read a command and execute it.
- * @s: The command to read.
- * @name: the name of the program
- * Return: 0 on success, 2 if the command is "exit" or error code on failure.
+ * command_read - Reads a command from stdin
+ * @s: The command to read
+ * Return: 0 on success, 1 on failure
  */
-int command_read(char *s, char *name)
+
+int command_read(char *s)
 {
 	int i;
 	char *token = NULL;
@@ -57,65 +52,71 @@ int command_read(char *s, char *name)
 
 	if (strcmp(s, "exit") == 0)
 		return (2);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' && s[i+1] == ' ')
+			return (0);
+	}
 	if (strcmp(s, "env") == 0)
 		return (_printenv());
-
-	i = 0;
 	token = strtok(s, " ");
+	i = 0;
 	while (token != NULL && i < 100)
 	{
-		cmd_array[i++] = token;
+		cmd_array[i] = token;
+		i++;
 		token = strtok(NULL, " ");
 	}
-
 	cmd_array[i] = NULL;
-
-	return (execute(cmd_array, name));
+	return (execute(cmd_array));
 }
 
 /**
- * execute - Function to execute a command.
- * @cmd_arr: The command to execute.
- * @name: the name of the program
- * Return: 0 on success, or the error code on failure.
+ * execute - Executes a command
+ * @cmd_arr: The command to execute
+ * Return: 0 on success, 1 on failure
  */
-int execute(char *cmd_arr[], char *name)
+
+int execute(char *cmd_arr[])
 {
 	pid_t pid;
 	char *exe_path;
 	int status;
+	char *name;
 
-	exe_path = command_path(cmd_arr[0]);
+	name = cmd_arr[0];
+	exe_path = command_path(name);
 	if (exe_path == NULL)
 	{
-		fprintf(stderr, "%s: %s: not found", name, cmd_arr[0]);
+		write(1, name, strlen(name));
+		write(1, ": not found\n", 12);
 		return (3);
 	}
-
 	pid = fork();
 	if (pid < 0)
 	{
-		perror("Error....");
+		perror("Error at creating a child process\n");
 		return (-1);
 	}
-
 	if (pid > 0)
-		wait(&status);
-	else
 	{
-		if (environ)
+		wait(&status);
+		if (WIFEXITED(status))
 		{
-			execve(exe_path, cmd_arr, environ);
-			exit(1);
-		}
-		else
-		{
-			execve(exe_path, cmd_arr, NULL);
-			exit(1);
+			int exit_status = WEXITSTATUS(status);
+			if (exit_status != 0)
+			{
+				printf("Command '%s' failed with exit status %d\n", name, exit_status);
+				return (exit_status);
+			}
 		}
 	}
-
+	else if (pid == 0)
+	{
+		execve(exe_path, cmd_arr, environ);
+		perror("Error");
+		exit(1);
+	}
 	free(exe_path);
-
 	return (0);
 }
