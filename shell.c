615e90a076edd1d5766d53ee24426b2e369c58c5
Arbes14 <arbesavdiaj14@gmail.com>
2023-12-20 00:41:07 -0800

    fix shell.c

--- shell.c
+++ shell.c
@@ -1,44 +1,6 @@
 #include "shell.h"
 
 /**
- * main - Entry point
- * @argc: The number of arguments
- * @argv: The arguments
- * Return: 0 on success, 1 on failure
- */
-
-int main(int __attribute__((unused)) argc, char *argv[])
-{
-	char *line = NULL;
-	size_t buf_size = 0;
-	ssize_t characters = 0;
-
-	name = argv[0];
-	while (1)
-	{
-		if (isatty(STDIN_FILENO) == 1)
-			write(1, "$ ", 2);
-		characters = getline(&line, &buf_size, stdin);
-		if (characters == -1)
-		{
-			if (isatty(STDIN_FILENO) == 1)
-				write(1, "\n", 1);
-			break;
-		}
-		if (line[characters - 1] == '\n')
-			line[characters - 1] = '\0';
-		if (*line == '\0')
-			continue;
-		if (strcmp(line, "exit") == 0)
-			break;
-		if (command_read(line) == 2)
-			break;
-	}
-	free(line);
-	return (0);
-}
-
-/**
  * command_read - Reads a command from stdin
  * @s: The command to read
  * Return: 0 on success, 1 on failure
@@ -52,11 +14,6 @@ int command_read(char *s)
 
 	if (strcmp(s, "exit") == 0)
 		return (2);
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == ' ' && s[i+1] == ' ')
-			return (0);
-	}
 	if (strcmp(s, "env") == 0)
 		return (_printenv());
 	token = strtok(s, " ");
@@ -64,8 +21,8 @@ int command_read(char *s)
 	while (token != NULL && i < 100)
 	{
 		cmd_array[i] = token;
-		i++;
 		token = strtok(NULL, " ");
+		i++;
 	}
 	cmd_array[i] = NULL;
 	return (execute(cmd_array));
@@ -82,41 +39,67 @@ int execute(char *cmd_arr[])
 	pid_t pid;
 	char *exe_path;
 	int status;
-	char *name;
 
-	name = cmd_arr[0];
-	exe_path = command_path(name);
+	exe_path = command_path(cmd_arr[0]);
 	if (exe_path == NULL)
 	{
-		write(1, name, strlen(name));
-		write(1, ": not found\n", 12);
-		return (3);
+		fprintf(stderr, "Command not found\n");
+		return (1);
 	}
 	pid = fork();
 	if (pid < 0)
 	{
 		perror("Error at creating a child process\n");
-		return (-1);
+		exit (1);
 	}
 	if (pid > 0)
 	{
 		wait(&status);
 		if (WIFEXITED(status))
-		{
-			int exit_status = WEXITSTATUS(status);
-			if (exit_status != 0)
-			{
-				printf("Command '%s' failed with exit status %d\n", name, exit_status);
-				return (exit_status);
-			}
-		}
+			exit(2);
 	}
-	else if (pid == 0)
+	if (pid == 0)
 	{
 		execve(exe_path, cmd_arr, environ);
 		perror("Error");
 		exit(1);
 	}
 	free(exe_path);
+	return (0);
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of arguments
+ * @argv: The arguments
+ * Return: 0 on success, 1 on failure
+ */
+
+int main(void)
+{
+	char *line = NULL;
+	size_t buf_size = 0;
+	ssize_t characters = 0;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) == 1)
+			write(1, "$ ", 2);
+		characters = getline(&line, &buf_size, stdin);
+		if (characters == -1)
+		{
+			if (isatty(STDIN_FILENO) == 1)
+				write(1, "\n", 1);
+			break;
+		}
+		if (line[characters - 1] == '\n')
+			line[characters - 1] = '\0';
+		trim_whitespace(line);
+		if (*line == '\0')
+			continue;
+		if (command_read(line) == 2)
+			break;
+	}
+	free(line);
 	return (0);
 }
